50dc8bfa25b1f815286582f1733d41394cfdd784
Eyang0612 <eddieyang0612@gmail.com>
2024-01-04 11:58:38 -0800

    added HomePage Dir

--- client/src/HomePage/Home.jsx
+++ client/src/HomePage/Home.jsx
@@ -1,9 +1,10 @@
 import Header from "./Header";
-
+import HomeBody from "./HomeBody"
 export default function Home(){
     return(
         <>
         <Header/>
+        <HomeBody/>
         </>
     )
 }
