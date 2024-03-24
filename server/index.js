2ce208ae4a30017ecaf3faa009b161548fa2c788
Eyang0612 <eddieyang0612@gmail.com>
2024-03-24 05:21:57 -0400

    Change index.js cors credentials

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const Image = require('./models/images')
 const app = express();
 app.use(cors({
   origin: process.env.REACT_APP_API_URL, // Replace with your frontend's URL
-  credentials: true
+  credentials: false
 }));
 
 app.use(express.json())
